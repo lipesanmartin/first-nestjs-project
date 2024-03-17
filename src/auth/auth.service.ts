5940b36c1ae2f15dc42181b44eaf40ae0075f388
Felipe Sanmartin <lipesanmartin@hotmail.com>
2024-03-17 02:00:15 -0300

    signup implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from "@nestjs/common";
-import { User, Bookmark } from "@prisma/client";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class AuthService {
@@ -8,11 +10,33 @@ export class AuthService {
 
   }
 
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    // save the new user in db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+          name: dto.name
+        }
+      });
+      delete user.hash;
+      return user;
+    }
+    catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+    }
+  }
+
   login() {
     return "This action logs a user in";
   }
 
-  signup() {
-    return "This action signs a user up";
-  }
+
 }
